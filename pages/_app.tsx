faac19009b39c631bbb64cf3b6d352fa28aa840e
aldomingues <andre.domingues@edu.unifil.br>
2022-12-05 04:05:13 -0300

    old integrated crud removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { GlobalProvider } from 'context/GlobalContext'
-import Header from '@components/layout/header'
-import DevBtn from '@components/layout/dev_btn'
-import { environment } from '@environment'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const enableDev = environment.enable_dev_options;
-  
+function MyApp({ Component, pageProps }: AppProps) {  
   return (
     <GlobalProvider>
-      {/* <Header/> */}
       <Component {...pageProps} />
 
-    {enableDev && <DevBtn/>}
     </GlobalProvider>
   )
 }
--- pages/api/api.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-// import { getAPIClient } from './axios'
-
-// export const api = getAPIClient();
-export const prisma = new PrismaClient();
